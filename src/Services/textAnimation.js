b7c6edced9c9f3651627865936213fa3ea937aa3
Wojtec <poninski.w@gmail.com>
2020-07-06 22:22:39 +0200

    animation

--- src/Services/textAnimation.js
+++ src/Services/textAnimation.js
@@ -1,71 +1,82 @@
- let animation = () => {
+let animation = () => {
     let text = document.querySelector(".hi");
     let leterArr = [...text.textContent];
+    console.log("object", leterArr);
+
     text.textContent = "";
     leterArr.map(char => {
-        return  (text.innerHTML += `<span>${char}</span>`)
+        return (text.innerHTML += `<span>${char}</span>`)
     })
-    
+
     let leter = 0;
-    let timer = setInterval(()=>{
-            let span = text.querySelectorAll('span')[leter];
-            span.classList.add('fade');
-            leter++;
-        if(leter === leterArr.length){
+    let timer = setInterval(() => {
+        let span = text.querySelectorAll('span')[leter];
+        span
+            .classList
+            .add('fade');
+
+        leter++;
+        if (leter === leterArr.length) {
             clearInterval(timer);
             myName();
             return;
         }
-       
+
     }, 100);
 
-
 }
-let myName = () =>{
+let myName = () => {
     let text = document.querySelector(".myName");
     let leterArr = [...text.textContent];
     text.textContent = "";
-    leterArr.map(char =>{
-        return (text.innerHTML += `<span>${char}</span>`)
-     })
-     let leter = 0;
-     let timer = setInterval(()=>{
-         let span = text.querySelectorAll('span')[leter];
-         span.classList.add('fade');
-         if(leter >= 12){
-             span.classList.add('white');
-         }
-         leter++;
-         if(leter === leterArr.length){
-             clearInterval(timer);
-             im();
-             return;
-         }
-
-        },100)
-}
-let im = () =>{
-    let text = document.querySelector(".im");
-    let leterArr = [...text.textContent];
-    text.textContent = "";
-    console.log("object", leterArr);
-
-    leterArr.map(char =>{
+    leterArr.map(char => {
         return (text.innerHTML += `<span>${char}</span>`)
     })
     let leter = 0;
-    let timer = setInterval(()=>{
+    let timer = setInterval(() => {
         let span = text.querySelectorAll('span')[leter];
-        span.classList.add('fade');
-        if(leter >= 18){
-            span.classList.add('white');
+        span
+            .classList
+            .add('fade');
+        if (leter >= 12) {
+            span
+                .classList
+                .add('white');
         }
         leter++;
-        if(leter === leterArr.length){
+        if (leter === leterArr.length) {
+            clearInterval(timer);
+            im();
+            return;
+        }
+
+    }, 100)
+}
+let im = () => {
+    let text = document.querySelector(".im");
+    let leterArr = [...text.textContent];
+    text.textContent = "";
+
+    leterArr.map(char => {
+        return (text.innerHTML += `<span>${char}</span>`)
+    })
+    let leter = 0;
+    let timer = setInterval(() => {
+        let span = text.querySelectorAll('span')[leter];
+        span
+            .classList
+            .add('fade');
+        if (leter >= 14) {
+            span
+                .classList
+                .add('white');
+        }
+        leter++;
+        if (leter === leterArr.length) {
             clearInterval(timer);
             return;
         }
-    },100);
+    }, 100);
 }
 
 export default animation;

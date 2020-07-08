29acbdc5cd850385d97d4ccbd023a52f514001e1
Wojtec <poninski.w@gmail.com>
2020-07-09 01:39:26 +0200

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
 
+    faBars,
 
+  } from "@fortawesome/free-solid-svg-icons";
 class Navbar extends Component {
-
+burger = () =>{
+    let navbarContainer = document.querySelector('.navbarContainer');
+    if(navbarContainer.style.display === "flex"){
+        navbarContainer.style.display = "none";
+    } else {
+        navbarContainer.style.display ="flex";
+    }
+}
     render(){
         return(
-            <nav>
-                <div className="navbar">
-                    <ul>
-                        <li>
+            <nav className="navbar">
+            <a className="logo">Poninski</a>
+                <div className="navbarContainer" >
+                        <a className="navAbout">
                             About
-                        </li>
-                        <li>
+                        </a>
+                        <a className="navProjects">
                             Projects
-                        </li>
-                        <li>
+                        </a>
+                        <a className= "navContact">
                             Contact
-                        </li>
-                    </ul>
+                        </a>
                 </div>
+                <a className="navbarIcon" onClick={()=>{this.burger()}}>
+                <FontAwesomeIcon icon={faBars} />
+                </a>
             </nav>
         )
     }

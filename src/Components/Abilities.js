3395ae9e31ecf6b130de7ae95e723366fdae3025
Wojtec <poninski.w@gmail.com>
2020-07-20 23:27:49 +0200

    Abilities p

--- src/Components/Abilities.js
+++ src/Components/Abilities.js
@@ -17,21 +17,27 @@ import {
             <h1>Abilities</h1>
         <div className="faCode">
         <FontAwesomeIcon icon={faCode} size="x2" />
+        <p>About code</p>
       </div>
       <div className="faMobile">
         <FontAwesomeIcon icon={faMobile} size="x2" />
+        <p>About mobile</p>
       </div>
       <div className="faCogs">
         <FontAwesomeIcon icon={faCogs} size="x2" />
+        <p>About Back-End</p>
       </div>
       <div className="faDatabase">
         <FontAwesomeIcon icon={faDatabase} size="x2" />
+        <p>About Database</p>
       </div>
-      <div className="faDatabase">
+      <div className="faCloud">
         <FontAwesomeIcon icon={faCloud} size="x2" />
+        <p>About Cloud</p>
       </div>
-      <div className="faDatabase">
+      <div className="faPayment">
         <FontAwesomeIcon icon={faDollarSign} size="x2" />
+        <p>About Payment Service</p>
       </div>
             </div>
         )

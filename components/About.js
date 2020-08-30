4de8a7dc81ffa430ed1523fd87cf36ed5e2b1891
Wojtec <poninski.w@gmail.com>
2020-08-30 14:29:20 +0200

    on

--- components/About.js
+++ components/About.js
@@ -30,16 +30,16 @@ const About = (props) => {
                     </Col>
                     <Col md="8">
                     <div className="about-content-description">
+                    <div className="about-content-description-dots">
+                            <span className="about-content-description-dots-dot"></span>
+                            <span className="about-content-description-dots-dot"></span>
+                            <span className="about-content-description-dots-dot"></span>
+                        </div>
                         <p>In October 2019 I decided to being Fullstack developer. Everyday I hard working to being closer to accomplish this goal.</p>
                         <p>I am finished Master Assembler School of Software Engineering in Barcelona.</p>
                         <p>I choosed this path of career because of my passions and interests. Creative creating new things and looking how they develop from nothing to something big gives me a lot of satisfaction and energy to carry on.</p>
                         <p>Curiosity how made changes and improvement will affect on final form of my work are something what always accompanies me during creating and development. Also this curiosity together with self-satisfaction are something what the most exiciting me in whole creating process. Currently I am during creating Clinic reservation system app React, Next.js, Node.js, Express and no relational database Mongodb.</p>
                         <p> Check my projects and personally see what I can do.</p>
-                        <div className="about-content-description-dots">
-                            <span className="about-content-description-dots-dot"></span>
-                            <span className="about-content-description-dots-dot"></span>
-                            <span className="about-content-description-dots-dot"></span>
-                        </div>
                     </div>
                     </Col>
                 </Row>      
--- components/Hero.js
+++ components/Hero.js
@@ -5,28 +5,29 @@ const Hero = (props) => {
 
     return(
             <Row >
-                <Col xs="12" className ="background-image">
-                <Container>
-
-                    <section className="hero-content">
-                            <h1 className="hero-content-title">
-                                <span className="hero-content-title-decoration"></span>
-                                <span className="hero-content-title-text">CREATE</span>
-                                <span className="hero-content-title-decoration"></span>
-                            </h1>
-                            <p className="hero-content-subtitle">YOUR OWN WEBSITE</p>
-                            <div className="hero-content-buttons">
-                                <a className="hero-content-buttons-btn" href="#">Projects</a>
-                                <a className="hero-content-buttons-btn" href="#">Skills</a>
+                <Col xs="12">
+                    <section className="background-image">
+                    <Container>
+                            <div className="hero-content">
+                                <h1 className="hero-content-title">
+                                    <span className="hero-content-title-decoration"></span>
+                                    <span className="hero-content-title-text">CREATE</span>
+                                    <span className="hero-content-title-decoration"></span>
+                                </h1>
+                                <p className="hero-content-subtitle">YOUR OWN WEBSITE</p>
+                                <div className="hero-content-buttons">
+                                    <a className="hero-content-buttons-btn" href="#">Projects</a>
+                                    <a className="hero-content-buttons-btn" href="#">Skills</a>
+                                </div>
+                                <div className="hero-content-description">Learn more about what I do</div>
+                                <div className="hero-content-arrow">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
                             </div>
-                            <div className="hero-content-description">Learn more about what I do</div>
-                            <div className="hero-content-arrow">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                    </section>
                     </Container>
+                    </section>
                 </Col>            
             </Row>
     )

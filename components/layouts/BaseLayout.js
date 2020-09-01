17e65ff48aeff37eb4128bd64e75c00972869959
Wojtec <poninski.w@gmail.com>
2020-09-01 22:06:52 +0200

    slate text editor

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import Header from '../shared/Header';
+import Head from 'next/head';
 
 const BaseLayout = (props) => {
 const { children, className, isAuthenticated, user } = props;
 const  headerType  = props.headerType || 'default';
 
     return(
+        <>
+        <Head>
+        <title>Wojtek Poninski</title>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
+        </Head>
         <div className="layout-container">
             <Header className={`port-nav-${headerType}`} isAuthenticated={ isAuthenticated } user={user} />
                 <main className={`cover ${className}`}>
@@ -13,6 +20,7 @@ const  headerType  = props.headerType || 'default';
                     </div>
                 </main>
         </div>
+        </>
     )
 }
 
--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,121 @@
 import React, { Component } from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
-
+import { Container , Row, Col} from 'reactstrap';
+import { Link } from '../routes';
 class Blog extends Component {
     render(){
         return(
             <>
-            <BaseLayout {...this.props.auth}>
-                <BasePage>
-                    <h1>Hi I'm Blog page</h1>
+            <BaseLayout {...this.props.auth} headerType={'landing'} className="blog-listing-page">
+                <div className="masthead" style={{"backgroundImage": "url('/images/home-bg.jpg')"}}>
+                    <div className="overlay"></div>
+                    <Container>
+                        <div className="row">
+                            <div className="col-lg-8 col-md-10 mx-auto">
+                                <div className="site-heading">
+                                    <h1>Fresh Blogs</h1>
+                                    <span className="subheading">Programming, travelling ...</span>
+                                </div>
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+                <BasePage className="blog-body">
+                    <Row>
+                        <Col md="10" lg="8" className="mx-auto">
+                            {
+                            <>
+                                <div  className="post-preview">
+                                    <Link route={`/blogs/blogId`}>
+                                    <a>
+                                        <h2 className="post-title">
+                                        Very Nice Blog Post
+                                        </h2>
+                                        <h3 className="post-subtitle">
+                                        How I Start Porgramming...
+                                        </h3>
+                                    </a>
+                                    </Link>
+                                    <p className="post-meta">Posted by
+                                    <a href="#">  Wojtek Poninski </a>
+                                    </p>
+                                </div>
+                                <hr></hr>
+                                <div  className="post-preview">
+                                    <Link route={`/blogs/blogId`}>
+                                    <a>
+                                        <h2 className="post-title">
+                                        Very Nice Blog Post
+                                        </h2>
+                                        <h3 className="post-subtitle">
+                                        How I Start Porgramming...
+                                        </h3>
+                                    </a>
+                                    </Link>
+                                    <p className="post-meta">Posted by
+                                    <a href="#">  Wojtek Poninski </a>
+                                   </p>
+                                </div>
+                                <hr></hr>
+                                <div  className="post-preview">
+                                    <Link route={`/blogs/blogId`}>
+                                    <a>
+                                        <h2 className="post-title">
+                                        Very Nice Blog Post
+                                        </h2>
+                                        <h3 className="post-subtitle">
+                                        How I Start Porgramming...
+                                        </h3>
+                                    </a>
+                                    </Link>
+                                    <p className="post-meta">Posted by
+                                    <a href="#"> Wojtek Poninski </a>
+                                   </p>
+                                </div>
+                                <hr></hr>
+                            </>
+                            }
+                            <div className="clearfix">
+                                <a href="#" className="btn btn-primary float-right">Older Posts &rarr;</a>
+                            </div>
+                        </Col>
+                    </Row>
+                    <footer>
+                    <Container>
+                        <Row>
+                        <div className="col-lg-8 col-md-10 mx-auto">
+                            <ul className="list-inline text-center">
+                            <li className="list-inline-item">
+                                <a href="#">
+                                <span className="fa-stack fa-lg">
+                                    <i className="fas fa-circle fa-stack-2x"></i>
+                                    <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="#">
+                                <span className="fa-stack fa-lg">
+                                    <i className="fas fa-circle fa-stack-2x"></i>
+                                    <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                                </span>
+                                </a>
+                            </li>
+                            <li className="list-inline-item">
+                                <a href="#">
+                                <span className="fa-stack fa-lg">
+                                    <i className="fas fa-circle fa-stack-2x"></i>
+                                    <i className="fab fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                                </a>
+                            </li>
+                            </ul>
+                            <p className="copyright text-muted">Copyright &copy; Wojtek Poninski 2020</p>
+                        </div>
+                        </Row>
+                    </Container>
+                    </footer>
                 </BasePage>
             </BaseLayout>
             </>

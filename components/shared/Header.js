5da6f2aa342820bf73eddd2236ecf903c36aeaa3
Wojtec <poninski.w@gmail.com>
2020-09-25 23:46:55 +0200

    navbar animations

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import auth0 from '../../services/auth0';
-import Link from 'next/link';
+import Link from '../../helpers/nextActiveNav';
 
 import {
   Collapse,
@@ -13,56 +13,79 @@ import {
 } from 'reactstrap';
 
 const BsNavLink = (props) => {
-const { route, title } = props;
-  return (
-    <Link href={route}>
-        <a className="nav-link port-navbar-link">{title}</a>
-    </Link>
-  )
+  const { route, title } = props;
+    return (
+      <Link activeClassName="active"  href={route}>
+        <a className={ "nav-link port-navbar-link" }>{title}</a>
+      </Link>
+    )
 }
 
 const Login = () => {
-    return (
-      <span 
-        onClick={auth0.login} 
-        className="nav-link port-navbar-link pointer">
-        Login
-      </span>
-    )
-
-  
+  return (
+    <span 
+      onClick={auth0.login} 
+      className="nav-link port-navbar-link pointer">
+      Login
+    </span>
+  )
 }
 
 const Logout = () => {
   return (
     <span 
-     onClick={auth0.logout } 
-     className="nav-link port-navbar-link pointer">
-     Logout
-     </span>
+      onClick={auth0.logout } 
+      className="nav-link port-navbar-link pointer">
+      Logout
+    </span>
   );
 }
-const NavbarComponent = (props) => {
 
-  const [headerText, setHeaderText] = useState(false);
+const NavbarComponent = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  const { isAuthenticated, className }  = props;
+
+  const [headerText, setHeader] = useState(false);
+
     useEffect(() => {
       const header = document.getElementById("navbar");
+      const navTags = document.getElementsByClassName("navbar-nav");
+      console.log(navTags);
       const scrollCallBack = window.addEventListener("scroll", () => {
-        console.log(window.pageYOffset);
-        if (window.pageYOffset > 80) {
+
+        if(window.pageYOffset === 0 || window.pageYOffset < 860){
+          navTags[0].childNodes[1].childNodes[0].classList.remove("active");
+          navTags[0].childNodes[0].childNodes[0].classList.add("active");
+
+        }
+
+        if(window.pageYOffset > 860 || window.pageYOffset < 2100) {
+          navTags[0].childNodes[0].childNodes[0].classList.remove("active");
+          navTags[0].childNodes[1].childNodes[0].classList.add("active");
+
+        }
+
+        if(window.pageYOffset > 2100) {
+          navTags[0].childNodes[1].childNodes[0].classList.remove("active");
+          navTags[0].childNodes[2].childNodes[0].classList.add("active");
+
+        }
+
+        if (window.pageYOffset >= 80) {
           header.classList.add("navbar-sticky");
           header.classList.remove("absolute");
-
-            if (headerText !== true) {
-              setHeaderText(true);
-            }
+      
+              setHeader(true);
+            
         } else {
           header.classList.remove("navbar-sticky");
           header.classList.add("absolute");
 
-            if (headerText !== false) {
-              setHeaderText(false);
-            }
+              setHeader(false);
+            
         }
       });
 
@@ -71,20 +94,21 @@ const NavbarComponent = (props) => {
       };
 
     }, []);
-
-
-
-
-
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
-  const  { isAuthenticated, className }  = props;
   
   return (
     <>
       <Navbar id="navbar" className={`port-navbar port-nav-base absolute ${className}`} light expand="md">
         <Container>
-            <NavbarBrand  className="port-navbar-brand" href="/"><span className="logo">W</span> Poninski</NavbarBrand>
+            <NavbarBrand  className="port-navbar-brand" href="/">
+              <div className="logo-container">
+                <div className={headerText ? "logo-container-logo navbar-rotate" : "logo-container-logo"}>
+                  W
+                </div>
+                <div className="logo-container-text">
+                  Poninski
+                </div>
+              </div>
+            </NavbarBrand>
             <NavbarToggler onClick={toggle} />
               <Collapse isOpen={isOpen} navbar>
                 <Nav className="ml-auto" navbar>

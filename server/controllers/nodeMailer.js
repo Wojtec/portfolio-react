0ef353536f62827c746da98c0848a3fe10f50580
wojtec <poninski.w@gmail.com>
2020-10-22 00:05:23 +0200

    heroku update

--- server/controllers/nodeMailer.js
+++ server/controllers/nodeMailer.js
@@ -7,12 +7,12 @@ module.exports = {
 
         const email = await sendEmail(data);
         
-        if (!email) {
+        if (email) {
+            return res.status(200).send(email);
 
-            return res.status(500).send("Server problem");
         } 
 
-        return res.status(200).send(true);
+        return res.status(500).send("Server problem");
 
     }
 }

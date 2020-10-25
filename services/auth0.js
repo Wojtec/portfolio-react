4e9478be4f766636f0f486123a4cec184eeaa2b2
wojtec <poninski.w@gmail.com>
2020-10-25 17:32:57 +0100

    heroku update

--- services/auth0.js
+++ services/auth0.js
@@ -51,7 +51,7 @@ class Auth0 {
         Cookies.remove('expiresAt');
         
         this.auth0.logout({
-            returnTo: '',
+            returnTo: '/',
             clientID: process.env.NEXT_PUBLIC_CLIENTID
         });
 

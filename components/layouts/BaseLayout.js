636bf8df53e92cabb27cef022728e3661cab2b3a
wojtec <poninski.w@gmail.com>
2020-10-25 16:25:47 +0100

    heroku update

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -29,8 +29,7 @@ const  headerType  = props.headerType || 'default';
                 <meta name="theme-color" content="#ffffff"/>
                 <title>W. Poninski</title>
                 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
-                <script src='../../helpers/traker'></script>
-
+                <script dangerouslySetInnerHTML={{ __html: (function(v,i,s,a){if(!v._visaSettings){v._visaSettings={};}v._visaSettings["e0746b85-155a-11eb-b589-901b0edac50a"]={v:"0.3",s:"e0746b85-155a-11eb-b589-901b0edac50a",a:"1"};_v=i.getElementsByTagName("head")[0];_a=_v;_i=i.createElement("script");_s=_i;_s.defer="defer";_s.src=s+a+v._visaSettings["e0746b85-155a-11eb-b589-901b0edac50a"].v;_a.appendChild(_s);})(window,document,"//app-worker.visitor-analytics.io/main",".js?s=e0746b85-155a-11eb-b589-901b0edac50a&v=") }} />
             </Head>
             <div className="layout-container">
                 <Header className={`port-nav-${headerType}`} isAuthenticated={ isAuthenticated } user={user}/>

93c131c245eb5f5f37bcd455ebf3e743fed6ad87
wojtec <poninski.w@gmail.com>
2020-10-25 16:21:35 +0100

    heroku update

--- helpers/traker.js
+++ helpers/traker.js
@@ -1,36 +1,29 @@
+const Tracker = dynamic(() => (Tracking())(
+    window,
+    document,
+    "//app-worker.visitor-analytics.io/main",
+    ".js?s=e0746b85-155a-11eb-b589-901b0edac50a&v="
+), {ssr: false})
 
-const Tracker = dynamic(
-    () => Tracking(),
-    { ssr: false }
-  )
+function Tracking(v, i, s, a) {
+    if (!v._visaSettings) {
+        v._visaSettings = {};
+    }
+    v._visaSettings["e0746b85-155a-11eb-b589-901b0edac50a"] = {
+        v: "0.3",
+        s: "e0746b85-155a-11eb-b589-901b0edac50a",
+        a: "1"
+    };
+    _v = i.getElementsByTagName("head")[0];
+    _a = _v;
+    _i = i.createElement("script");
+    _s = _i;
+    _s.defer = "defer";
+    _s.src = s + a + v
+        ._visaSettings["e0746b85-155a-11eb-b589-901b0edac50a"]
+        .v;
+    _a.appendChild(_s)
 
-
-
-function Tracking() {
-    (function (v, i, s, a) {
-        if (!v._visaSettings) {
-            v._visaSettings = {};
-        }
-        v._visaSettings["e0746b85-155a-11eb-b589-901b0edac50a"] = {
-            v: "0.3",
-            s: "e0746b85-155a-11eb-b589-901b0edac50a",
-            a: "1"
-        };
-        _v = i.getElementsByTagName("head")[0];
-        _a = _v;
-        _i = i.createElement("script");
-        _s = _i;
-        _s.defer = "defer";
-        _s.src = s + a + v
-            ._visaSettings["e0746b85-155a-11eb-b589-901b0edac50a"]
-            .v;
-        _a.appendChild(_s);
-    })(
-        window,
-        document,
-        "//app-worker.visitor-analytics.io/main",
-        ".js?s=e0746b85-155a-11eb-b589-901b0edac50a&v="
-    )
 }
 
 export default Tracker();

aa7df337a201c5fff9edd2d3d65eb05454882c32
wojtec <poninski.w@gmail.com>
2020-10-26 22:41:14 +0100

    projects cards animations update

--- components/project/projectCard.js
+++ components/project/projectCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
+
 import { 
     CardBody, 
     CardText, 
@@ -25,16 +27,35 @@ class ProjectCard extends Component {
         const { project, children} = this.props;
         const { isOpen } = this.state;
         return(
-            <div  onClick={this.handleTaggle}>
+            <div>
             <ProjectCardDetail isOpen={isOpen} project={project} toggle={this.handleTaggle}/>
             <Card className="project-card">
-            <div className="project-card-image">
-            <CardImg className="project-card-image-img" src={project.img} alt="Card image cap" />
+            <div onClick={this.handleTaggle} className="project-card-image">
+            <CardImg id="card-image" className="project-card-image-img" src={project.img} alt="Card image cap" />
             </div>
+            <div id="card-body-container" className="card-body-container">
                 <CardBody >
-                    <CardTitle className="project-card-title">{project.title}</CardTitle>
-                    <CardText className="project-card-text">{project.subtitle}</CardText>
+                        <CardTitle className="project-card-title">{project.title}</CardTitle>
+                        <CardText className="project-card-text">{project.subtitle}</CardText>
+                        <div className="project-card-decoration-long"></div>
+
+                        <CardText className="project-card-main">{project.description}</CardText>
+
+                        <div className="project-card-decoration-long"></div>
+                        <div className="card-body-buttons">
+                        <Link
+                           href="https://music-poninski.herokuapp.com"
+                            >
+                              <a className={ `card-body-buttons-btn` }>Demo</a>  
+                        </Link>
+                        <Link
+                            href="/projects"
+                            >
+                               <a className={ `card-body-buttons-btn` }> More </a> 
+                        </Link>                      
+                        </div>
                 </CardBody>
+            </div>
                 {children}
             </Card>
             </div>

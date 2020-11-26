9eae3d4ac77e2277df792ecfe0e3ac7e20a14b00
wojtec <poninski.w@gmail.com>
2020-11-26 22:00:49 +0100

    url

--- actions/index.js
+++ actions/index.js
@@ -1,66 +1,66 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import axios from "axios";
+import Cookies from "js-cookie";
 
-import { getCookieFromReq } from '../helpers/utils';
+import { getCookieFromReq } from "../helpers/utils";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
-    timeout: 3000
-})
+  baseURL: "https://poninski.herokuapp.com/api/v1",
+  timeout: 3000,
+});
 
 const rejectPromise = (resError) => {
-    let error = {};
+  let error = {};
 
-    if (resError && resError.response && resError.response.data) {
-        error = resError.response.data;
-    } else {
-        error = resError;
-    }
+  if (resError && resError.response && resError.response.data) {
+    error = resError.response.data;
+  } else {
+    error = resError;
+  }
 
-    return Promise.reject(error);
-}
+  return Promise.reject(error);
+};
 
 const setAuthHeader = (req) => {
-    const token = req ? getCookieFromReq(req, 'jwt') : Cookies.getJSON('jwt');
-    
-    return token ? ( { headers: {'Authorization': `Bearer ${token}`}} ) : ( undefined );
+  const token = req ? getCookieFromReq(req, "jwt") : Cookies.getJSON("jwt");
 
-}
+  return token ? { headers: { Authorization: `Bearer ${token}` } } : undefined;
+};
 
-// PROJECTS 
+// PROJECTS
 
 export const getProjects = async () => {
-
-    return await axiosInstance.get('/projects').then(response => response.data);
-}
+  return await axiosInstance.get("/projects").then((response) => response.data);
+};
 
 export const createProject = async (projectData) => {
-    return await axiosInstance.post('/projects',projectData, setAuthHeader())
-    .then(response => response.data)
-    .catch(error => rejectPromise(error));
-}
+  return await axiosInstance
+    .post("/projects", projectData, setAuthHeader())
+    .then((response) => response.data)
+    .catch((error) => rejectPromise(error));
+};
 
 export const getProjectById = async (id) => {
-    return axiosInstance.get(`/projects/${id}`)
-    .then(response => response.data);
-}
+  return axiosInstance.get(`/projects/${id}`).then((response) => response.data);
+};
 
 export const updateProject = async (projectData) => {
-    return await axiosInstance.patch(`/projects/${projectData._id}`,projectData, setAuthHeader())
-    .then(response => response.data)
-    .catch(error => rejectPromise(error));
-}
+  return await axiosInstance
+    .patch(`/projects/${projectData._id}`, projectData, setAuthHeader())
+    .then((response) => response.data)
+    .catch((error) => rejectPromise(error));
+};
 
 export const deleteProject = (projectId) => {
-    return axiosInstance.delete(`/projects/${projectId}`, setAuthHeader()).then(response => response.data);
-}
+  return axiosInstance
+    .delete(`/projects/${projectId}`, setAuthHeader())
+    .then((response) => response.data);
+};
 
 //NODE MAILER
 
 export const sendMail = async (mailData) => {
-
-    return await axiosInstance.post('/mail', mailData)
-    .then(response => response.data)
-    .catch(error => rejectPromise(error));
-    
-}
+  return await axiosInstance
+    .post("/mail", mailData)
+    .then((response) => response.data)
+    .catch((error) => rejectPromise(error));
+};
